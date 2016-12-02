8938fc52d2107c14c9e4b5dd8110660a7dde8f3b
252717 <luohao0414@gmail.com>
2016-12-02 10:23:33 +0800

    完成下拉刷新Demo。

--- PullToRefreshTestMySelf/app/src/main/java/com/example/administrator/pulltorefreshtestmyself/MainActivity.java
+++ PullToRefreshTestMySelf/app/src/main/java/com/example/administrator/pulltorefreshtestmyself/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.administrator.pulltorefreshtestmyself;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RefreshableView mRefreshableView;
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    private String[] mItem = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        mRefreshableView = (RefreshableView) findViewById(R.id.refreshable_view);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItem);
+        mListView.setAdapter(mAdapter);
+        mRefreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mRefreshableView.finishRefreshing();
+            }
+        }, 0);
     }
 }
